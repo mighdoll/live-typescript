5b172991687f59da8ba50527f38680d1a5e66a38
lee mighdoll <lee@underneath.ca>
2023-05-30 21:08:53 -0700

    example code as prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 import { CodeEditor } from "./CodeExample.js";
 
+const exampleCode = `
+import { bufferI32 } from "thimbleberry";
+
+// withGpuDevice(main);
+console.log("hello world");
+
+async function main(device: GPUDevice): Promise<void> {
+  const srcData = [1, 2, 3, 4, 5, 6];
+  const src = bufferI32(device, srcData);
+  
+
+}
+
+`;
+
 export function App(): JSX.Element {
-  return <CodeEditor />;
+  return <CodeEditor code={exampleCode}/>;
 }
