9876e17f87c4f4daf5c26ca72d5c8c8706852571
lee mighdoll <lee@underneath.ca>
2023-05-30 21:09:02 -0700

    refactoring init a bit

--- src/CodeExample.tsx
+++ src/CodeExample.tsx
@@ -6,71 +6,45 @@ import "./codeExample.css";
 import webgpuTypes from "/node_modules/@webgpu/types/dist/index.d.ts?raw";
 import webgpuPackage from "/node_modules/@webgpu/types/package.json?raw";
 import thimbleberryPackage from "/node_modules/thimbleberry/package.json?raw";
+import stoneberryPackage from "/node_modules/stoneberry/package.json?raw";
+
 type Monaco = typeof monaco_editor;
 
-const modules = import.meta.glob("/node_modules/thimbleberry/dist/**/*.d.ts", {
-  as: "raw",
-  eager: true,
-});
-
-const exampleCode = `
-  import { labeledGpuDevice } from "thimbleberry";
-  
-  const device = labeledGpuDevice();
-  main(device);
-  
-  async function main(device: GPUDevice): Promise<void> {
-      document.body.innerText = "hello world";
+const thimbleberryTypes = import.meta.glob(
+  "/node_modules/thimbleberry/dist/**/*.d.ts",
+  {
+    as: "raw",
+    eager: true,
   }
-`;
+);
+
+const stoneberryTypes = import.meta.glob(
+  "/node_modules/stoneberry/dist/**/*.d.ts",
+  {
+    as: "raw",
+    eager: true,
+  }
+);
+// console.log(stoneberryTypes);
 
 interface CodeEditorProps {
   height?: number | string;
   width?: number | string;
   imports?: string[];
+  code?: string;
 }
 
 const defaults: CodeEditorProps = {
   height: "400px",
   width: "500px",
   imports: ["thimbleberry"],
+  code: "// hello world",
 };
-
-function initializeMonaco(monaco: Monaco) {
-  monaco.languages.typescript.typescriptDefaults.addExtraLib(
-    webgpuTypes,
-    `file:///node_modules/@webgpu/types/dist/index.d.ts`
-  );
-  monaco.languages.typescript.typescriptDefaults.addExtraLib(
-    webgpuPackage,
-    `file:///node_modules/@webgpu/types/package.json`
-  );
-  monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-    types: ["@webgpu/types"],
-    moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
-    module: monaco.languages.typescript.ModuleKind.ESNext,
-  });
-
-  monaco.languages.typescript.typescriptDefaults.addExtraLib(
-    thimbleberryPackage,
-    `file:///node_modules/thimbleberry/package.json`
-  );
-
-  for (const [path, source] of Object.entries(modules)) {
-    console.log(path);
-    monaco.languages.typescript.typescriptDefaults.addExtraLib(
-      source,
-      `file://${path}`
-    );
-  }
-
-  monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
-}
 
 export function CodeEditor(props: CodeEditorProps): JSX.Element {
   const monaco = useMonaco();
-  const { height, width, imports } = { ...defaults, ...props };
-  const [compiledCode, setCompiledCode] = useState(transpile(exampleCode));
+  const { height, width, code, imports } = { ...defaults, ...props };
+  const [compiledCode, setCompiledCode] = useState(transpile(code!));
 
   const options: monaco_editor.editor.IStandaloneEditorConstructionOptions = {
     readOnly: false,
@@ -89,7 +63,6 @@ export function CodeEditor(props: CodeEditorProps): JSX.Element {
     (value: any) => {
       const src = value as string;
       const compiled = transpile(src);
-      console.log({ compiled });
       setCompiledCode(compiled);
     },
     [setCompiledCode]
@@ -115,7 +88,7 @@ export function CodeEditor(props: CodeEditorProps): JSX.Element {
       <Editor
         {...{ height, width }}
         defaultLanguage="typescript"
-        defaultValue={exampleCode}
+        defaultValue={code}
         defaultPath="file:///index.ts"
         options={options}
         onChange={codeChange}
@@ -124,4 +97,42 @@ export function CodeEditor(props: CodeEditorProps): JSX.Element {
       <iframe srcDoc={html} className="codeExample"></iframe>
     </div>
   );
+}
+
+function initializeMonaco(monaco: Monaco) {
+  monaco.languages.typescript.typescriptDefaults.addExtraLib(webgpuTypes);
+  addLocalTsLib(monaco, webgpuTypes, `@webgpu/types/dist/index.d.ts`);
+  addLocalTsLib(monaco, webgpuPackage, `@webgpu/types/package.json`);
+
+  monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+    types: ["@webgpu/types"],
+    moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+    module: monaco.languages.typescript.ModuleKind.ESNext,
+  });
+
+  addLocalTsLib(monaco, thimbleberryPackage, `thimbleberry/package.json`);
+  // addTsLib(monaco, stoneberryPackage, `stoneberry/package.json`);
+
+  addTypes(monaco, thimbleberryTypes);
+  // addTypes(monaco, stoneberryTypes);
+
+  monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
+}
+
+function addTypes(monaco: Monaco, types: Record<string, string>) {
+  for (const [path, source] of Object.entries(types)) {
+    console.log("path:", path);
+    const filePath = `file://${path}`;
+    monaco.languages.typescript.typescriptDefaults.addExtraLib(
+      source,
+      filePath
+    );
+  }
+}
+
+/** add a typescript default library and set path in node modules */
+function addLocalTsLib(monaco: Monaco, source: string, path?: string) {
+  const filePath = path && `file:///node_modules/${path}`;
+  console.log("tslib path", filePath);
+  monaco.languages.typescript.typescriptDefaults.addExtraLib(source, filePath);
 }
