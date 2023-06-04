1fcf312ee146acc2037052bd79d24a283c2170e1
lee mighdoll <lee@underneath.ca>
2023-06-04 13:45:37 -0700

    import example code from installed pkg

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
 import React from "react";
 import { CodeEditor } from "./CodeExample.js";
 import { installStoneberryTypes } from "./StoneberryMonacoTypes.js";
-
-const exampleCode = `
-  import { bufferI32 } from "thimbleberry";
-  import { PrefixScan } from "stoneberry/scan";
-  import { renderTable, withGpuDevice } from "stoneberry-examples";
-
-  withGpuDevice(main);
-  console.log("hello world");
-
-  async function main(device: GPUDevice): Promise<void> {
-    const srcData = [1, 2, 3, 4, 5, 6];
-    const src = bufferI32(device, srcData);
-
-    const scanner = new PrefixScan({ device, src });
-    const inclusiveScan = await scanner.scan();
-
-    renderTable({ source: srcData, "inclusive scan": inclusiveScan });
-  }
-`;
+import exampleCode from "/node_modules/stoneberry-examples/src/simpleScan.ts?raw";
 
 export function App(): JSX.Element {
   const packages = ["thimbleberry", "stoneberry/scan", "stoneberry-examples"];
   return (
     <CodeEditor
-      {...{ packages }}
+      { ...{packages }}
       setupTypes={installStoneberryTypes}
       code={exampleCode}
     />
