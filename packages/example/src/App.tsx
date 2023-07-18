26a2e7fab28a40e98ac33feb4412f6dc6f43c961
lee mighdoll <lee@underneath.ca>
2023-07-18 14:05:29 -0700

    use thimbleberry in example

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,14 +1,18 @@
-import React from "react";
-import { ThimbleberryExample } from "./ThimbleberryExample";
 import "live-typescript/style.css";
+import { mapN } from "thimbleberry";
+import { ThimbleberryExample } from "./ThimbleberryExample";
 
 export function App(): JSX.Element {
   const codeLines = example.toString().split("\n");
-  const code = codeLines.slice(1, codeLines.length - 1).join("\n");
+  const codeImport = 'import {mapN} from "thimbleberry";\n';
+  const bodyLines = codeLines
+    .slice(1, codeLines.length - 1)
+    .map((s) => s.trim());
+  const code = [codeImport, ...bodyLines].join("\n");
   return <ThimbleberryExample {...{ code }} />;
 }
 
 function example(): void {
-  
-  document.body.innerHTML = `<div> hello world!</div>`;
+  const seq = mapN(10).join(" ");
+  document.body.innerHTML = `<div> ${seq} </div>`;
 }
