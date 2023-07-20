dbc1e18f4abcbd56d0753f38faf5ac5da37df89e
lee mighdoll <lee@underneath.ca>
2023-07-19 23:27:30 -0700

    tighten tests a bit

--- packages/rollup-test/test/plugin.test.ts
+++ packages/rollup-test/test/plugin.test.ts
@@ -13,7 +13,7 @@ test("load and patch thimbleberry", async () => {
 
   const pkg = "thimbleberry";
   const thimbUrl = resolveModule(pkg, rootUrl);
-  const {map} = await loadModule(thimbUrl, pkg);
+  const { map } = await loadModule(thimbUrl, pkg);
 
   // map contains thimbleberry bare reference map entry
   const keys = Object.keys(map);
@@ -24,24 +24,37 @@ test("load and patch thimbleberry", async () => {
   expect(hashedKeys.length).toBeGreaterThan(0);
 
   // internal imports are uniqified
-  const lines = map["thimbleberry"].split("\n");
-  const importLines = lines.filter((l) => l.includes("import"));
-  expect( importLines.length).toBeGreaterThan(2);
-  importLines.forEach(l => {
-    const hashImport = /-[a-z0-9]{7}/.test(l);
-    expect(hashImport).toBe(true);
-  });
+  verifyImportHashIds(map);
 });
 
 test("recursive thimbleberry import", async () => {
   const rootPath = path.join(process.env.PWD!, "package.json");
   const rootUrl = url.pathToFileURL(rootPath);
-
   const map = await recursiveImports("thimbleberry", rootUrl, new Set());
 
+  // recursive import got a child package
+  expect(Object.keys(map)).contains("@reactively/core");
+
+  // no require statements left behind
   Object.entries(map).forEach(([key, value]) => {
     if (value.includes("require")) {
       assert.fail(`require found in ${key}`);
     }
   });
+
+  // internal imports are uniqified
+  verifyImportHashIds(map);
 });
+
+function verifyImportHashIds(map: Record<string, string>): void {
+  Object.entries(map).forEach(([key, code]) => {
+    const lines = code.split("\n");
+    const importLines = lines.filter((l) => l.includes(' from "'));
+    importLines.forEach((line) => {
+      const hashImport = /-[a-z0-9]{7}/.test(line);
+      if (!hashImport) {
+        assert.fail(`non-uniqified import from ${key}: ${line}`);
+      }
+    });
+  });
+}
