930d152346b77919b1b2105e8529df42c602e805
lee mighdoll <lee@underneath.ca>
2023-07-20 20:47:43 -0700

    build example from local src

--- packages/example/vite.config.ts
+++ packages/example/vite.config.ts
@@ -1,7 +1,8 @@
+import tsConfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import remapImports from "rollup-plugin-remap-imports";
 
 export default defineConfig({
-  plugins: [react(), remapImports(process.env.PWD)],
+  plugins: [tsConfigPaths(), react(), remapImports(process.env.PWD)],
 });
---
+++ packages/live-typescript/src/index.ts
@@ -0,0 +1,3 @@
+export * from "./CodeExample.js";
+export * from "./Imports.js";
+export * from "./Transpile.js";
