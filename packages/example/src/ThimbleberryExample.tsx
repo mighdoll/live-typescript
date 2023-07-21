83cbe25d983b8b28a052cb76f4c82cece0247b5a
lee mighdoll <lee@underneath.ca>
2023-07-20 23:36:02 -0700

    add webgpu types too

--- packages/example/src/ThimbleberryExample.tsx
+++ packages/example/src/ThimbleberryExample.tsx
@@ -1,8 +1,10 @@
 import "react";
 import { CodeExample } from "live-typescript";
 import "live-typescript/style.css";
-import thimbleberry from "thimbleberry?remapImports";
+import thimbleberryCode from "thimbleberry?remapImports";
 import thimbleberryPackage from "../node_modules/thimbleberry/package.json?raw";
+import webgpuTypes from "../node_modules/@webgpu/types/dist/index.d.ts?raw";
+import webgpuPackage from "../node_modules/@webgpu/types/package.json?raw";
 
 const thimbleberryTypes = import.meta.glob(
   "/node_modules/thimbleberry/**/*.d.ts",
@@ -23,7 +25,7 @@ export function ThimbleberryExample(
   return (
     <CodeExample
       {...{
-        embeddedPackages: thimbleberry,
+        embeddedPackages: thimbleberryCode,
         typeFiles: createTypeFiles(),
         code,
       }}
@@ -32,13 +34,18 @@ export function ThimbleberryExample(
 }
 
 function createTypeFiles(): Record<string, string> {
-  const mapEntries = Object.entries(thimbleberryTypes).map(([path, text]) => {
+  const mapEntries = [
+    localPackage("thimbleberry/package.json", thimbleberryPackage),
+    localPackage(`@webgpu/types/dist/index.d.ts`, webgpuTypes),
+    localPackage(`@webgpu/types/package.json`, webgpuPackage),
+  ];
+  const typeEntries = Object.entries(thimbleberryTypes).map(([path, text]) => {
     return [`file://${path}`, text];
   });
-  mapEntries.push([
-    "file:///node_modules/thimbleberry/package.json",
-    thimbleberryPackage,
-  ]);
-  const map = Object.fromEntries(mapEntries);
+  const map = Object.fromEntries([...mapEntries, ...typeEntries]);
   return map;
+}
+
+function localPackage(path: string, code: string): [string, string] {
+  return [`file:///node_modules/${path}`, code];
 }
