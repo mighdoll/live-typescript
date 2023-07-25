47e406d95b42a7e084b1eb4815cb64cb875911b2
lee mighdoll <lee@underneath.ca>
2023-07-24 23:41:04 -0700

    rm old scripts

--- packages/example/vite.config.ts
+++ packages/example/vite.config.ts
@@ -1,16 +1,12 @@
 import tsConfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import remapImports from "rollup-plugin-remap-imports";
-import typeFiles from "rollup-plugin-typefiles";
 import sourceFiles from "rollup-plugin-sourcefiles";
 
 export default defineConfig({
   plugins: [
     tsConfigPaths(),
     react(),
-    remapImports(process.env.PWD),
-    typeFiles(process.env.PWD),
     sourceFiles(process.env.PWD),
   ],
 });
