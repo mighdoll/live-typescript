c7e51f88ae06e36164c2eb61994935950603af03
lee mighdoll <lee@underneath.ca>
2023-10-08 23:03:55 -0700

    utilities for finding package.json files

--- packages/rollup-plugin-sourcefiles/src/loadModule.ts
+++ packages/rollup-plugin-sourcefiles/src/loadModule.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import { moduleResolve } from "import-meta-resolve";
 import fs from "fs/promises";
 import module from "node:module";
@@ -8,7 +9,7 @@ const moduleCache = new Map<string, LoadedModule>();
 
 const importConditions = new Set(["node", "import"]);
 
-/** @return the local fs path for a given package name */
+/** @return the local fs path to the import entry point for a given package name */
 export function resolveModule(pkg: string, baseUrl: URL): URL {
   // use import-meta-resolve if possible so that we can match the
   // exported 'import:' entry in package.json packages rather than using the 'require:' entry
@@ -47,4 +48,40 @@ export async function cachedLoadModule(
   const result: LoadedModule = { contents, hashId };
   moduleCache.set(url.href, result);
   return result;
+}
+
+/** search up the path from a resolved module path to find package.json */
+export async function containingPackageJson(pkgPath: string): Promise<string> {
+  let dirPath = path.resolve(pkgPath, "..");
+  while (dirPath !== "/") {
+    const packageJsonPath = path.join(dirPath, "package.json");
+    const fStat = await fs.stat(packageJsonPath).catch(() => null);
+    if (fStat?.isFile()) {
+      return packageJsonPath;
+    }
+    dirPath = path.resolve(dirPath, "..");
+  }
+  throw new Error(`could not find package.json in ${pkgPath}`);
+}
+
+/** find the package json for a name package, either in this directory's node_modules
+ * or on a parent directory's node_modules */
+export async function packageJsonForPkg(
+  pkg: string,
+  basePath: string
+): Promise<string> {
+  const nodeModulesPath = path.join(basePath, "node_modules");
+  const nodeStat = await fs.stat(nodeModulesPath).catch(() => null);
+  if (nodeStat?.isDirectory()) {
+    const pkgJsonPath = path.join(nodeModulesPath, pkg, "package.json");
+    const pkgJsonStat = await fs.stat(pkgJsonPath).catch(() => null);
+    if (pkgJsonStat?.isFile()) {
+      return pkgJsonPath;
+    }
+  }
+  if (path.dirname(basePath) !== basePath) {
+    return packageJsonForPkg(pkg, path.dirname(basePath));
+  } else {
+    throw new Error(`could not find package.json for ${pkg} in ${basePath}`);
+  }
 }
